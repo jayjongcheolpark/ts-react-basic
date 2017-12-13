1c16b1bca10cbcc2212a64be023467a86caece76
Jay Park <jay.jongcheol.park@gmail.com>
2017-12-12 22:50:43 -0500

    Stateful React Components

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,38 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 
 interface AppProps { message: string }
-const App = (props: AppProps) => <div>{props.message}</div>
+const App = (props: AppProps) => {
+  return (
+    <div>
+      <ChildApp message={props.message} />
+    </div>
+  )
+}
 
+interface ChildAppProps { message: string }
+interface ChildAppState { count: number }
+
+class ChildApp extends React.Component<ChildAppProps, ChildAppState> {
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 0
+    }
+  }
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <div onClick={this.increment}>{this.props.message}</div>
+        <div>{this.state.count}</div>
+      </div>
+    )
+  }
+}
 ReactDOM.render(
   <App message="Hello world" />, document.getElementById('root'))
